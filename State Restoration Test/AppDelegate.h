cb18352ead299aa23a963c9231f2ffd525409735
Daniel Tull <dt@danieltull.co.uk>
2014-02-27 20:52:25 +0000

    Initial Commit

---
+++ State Restoration Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  State Restoration Test
+//
+//  Created by Daniel Tull on 27/02/2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
